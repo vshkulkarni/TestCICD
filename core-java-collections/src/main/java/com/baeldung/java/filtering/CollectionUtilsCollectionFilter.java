7cbd34a61e4c71f988930f785ed6744454702edd
vishnu <vshkulkarni@gmail.com>
2018-12-03 08:25:15 +0530

    rerewrwer

--- core-java-collections/src/main/java/com/baeldung/java/filtering/CollectionUtilsCollectionFilter.java
+++ core-java-collections/src/main/java/com/baeldung/java/filtering/CollectionUtilsCollectionFilter.java
@@ -6,7 +6,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.Predicate;
 
 public class CollectionUtilsCollectionFilter {
-
+ 
     static public Collection<Integer> findEvenNumbers(Collection<Integer> baseCollection) {
         Predicate<Integer> apacheEventNumberPredicate = item -> item % 2 == 0;
 
